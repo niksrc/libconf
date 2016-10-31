f1b8c7d2404f449cccd8570cc5347154cc77d3af
Nikhil Srivastava <niksrc@gmail.com>
2016-10-31 15:55:31 +0530

    modularize the api

--- index.js
+++ index.js
@@ -1,89 +1,28 @@
-var fs = require('fs');
-var path = require('path');
+const loadConfig = require('./lib/loadConfig');
+const api = require('./lib/api');
+const path = require('path');
 
-// Base Config Directory
-var baseDir = process.env.NODE_ENV_CONFIG_DIR || path.resolve(__dirname, './../config');
+// Config base directory in the project
+const configHome = process.env.NODE_ENV_CONFIG_DIR || path.resolve(process.cwd(), './config');
+/**
+ * Current Config Dir
+ * Checks For NODE_ENV otherwise fallback to default directory
+ */
+const env = process.env.NODE_ENV || 'default';
 
-// Current Config Dir
-// Checks For NODE_ENV otherwise fallback to default directory
-var env = process.env.NODE_ENV || 'default';
+let config = {
+	// Load default config
+	default: loadConfig(configHome, 'default')
+};
 
-var defaultConfig = getConfig(baseDir, 'default');
-var currentConfig = defaultConfig;
-// if env is not default load config for it
+// if env is not default also load config for it
 if (env !== 'default') {
- currentConfig = getConfig(baseDir, env);
+ config[env] = loadConfig(configHome, env);
 }
 
-/**
- * Returns a map of config type and values for a given directory
- * @param  {String} baseDir   Base directory of config
- * @param  {String} configDir Directory in which config files are present
- * @return {Object} config    A map config type and value pairs
- */
-function getConfig(baseDir, configDir) {
-	var configDir = path.join(baseDir, env)
-	var files;
-	// Load all config files
-	try {
-		files = fs.readdirSync(configDir);
-	} catch (error) {
-		files = [];
-		throw('Config Directory ' + configDir + ' can\'t be accessed');
-	}
-
-	return files
-		.reduce(function (acc, file) {
-			var configType = path.basename(file, '.js');
-			var filePath = path.join(configDir, file);
-			acc[configType] = require(filePath);
-			return acc;
-		}, {});
-}
-
-/**
- * Returns value of config key for a given type that falls back to default config
- * @param  {String} type Type of config
- * @param  {String} key  Key to be looked
- * @return {Anything}    Value of Key in given type
- */
-function getKey(type, key) {
-	// If key is present in current Config use it
-	if (currentConfig[type] && typeof currentConfig[type][key] !== 'undefined') {
-		return currentConfig[type][key];
-	}
-
-	// else fall back to default config
-	if (typeof defaultConfig[type] !== 'undefined') {
-		return defaultConfig[type][key];
-	}
-
-	// Invalid type
-	throw('Invalid config type');
-}
-
-/**
- * Returns full config for a given type
- * @param  {String} type Type of config
- * @param  {String} key  Key to be looked
- * @return {Anything}    Value of Key in given type
- */
-function getAll(type) {
-	// If key is present in current Config use it
-	if (typeof currentConfig[type] !== 'undefined') {
-		return currentConfig[type];
-	}
-
-	// else fall back to default config
-	if (typeof defaultConfig[type] !== 'undefined') {
-		return defaultConfig[type];
-	}
-
-	// Invalid type
-	throw('Invalid config type', type);
-}
-
+// bind default params and export
 module.exports = {
-	get: getKey,
-	getAll: getAll
-}
+	has: api.has.bind(null, env, config),
+	get: api.get.bind(null, env, config),
+	getAll: api.getAll.bind(null, env, config),
+};
