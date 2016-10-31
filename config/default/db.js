9b6144df598be1c2e8dfd7e997cf71eb98411049
Nikhil Srivastava <niksrc@gmail.com>
2016-10-31 15:54:43 +0530

    add test values

---
+++ config/default/db.js
@@ -0,0 +1,17 @@
+var mysql = {
+	user: 'roottoor',
+	password: 'secretsecret',
+	host: 'http://locahost/mariadb',
+	port: 1111
+}
+
+var cassandra = {
+	user: 'nik',
+	host: 'locahost',
+	port: '3321'
+}
+
+module.exports = {
+	mysql: mysql,
+	cassandra: cassandra
+}
---
+++ config/dev/db.js
@@ -0,0 +1,15 @@
+var mysql = {
+	user: 'root',
+	password: 'secret123',
+	host: 'http://hosteddb.com/mariadb',
+	port: 2234
+}
+
+var cassandra = {
+	user: 'nik'
+}
+
+module.exports = {
+	mysql: mysql,
+	cassandra: cassandra
+}
